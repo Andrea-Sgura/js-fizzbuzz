2c7be57e6b0904b429ded5cbc66a2fb8a16b88fc
Andrea-Sgura <andreasgura96@gmail.com>
2025-03-18 16:11:25 +0100

    [UPD] Inserted fizz and buzz variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 // 1 - Inserisco 2 variabili che contengono i numeri 1 e 100 e le inizializzo;
 let firstNumber = 1;
 let lastNumber = 100;
+
+// 2 - Inserisco le 2 variabili fizz e buzz contenenti i numeri 3 e 5 e le inizializzo;
+let fizz = 3;
+let buzz = 5;
