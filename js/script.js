a0779cf4aa5266078eb2c579f7a35ca2ca92ad93
Andrea-Sgura <andreasgura96@gmail.com>
2025-03-18 16:24:39 +0100

    [UPD] Created cicle "For", excercise done.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@ let lastNumber = 100;
 // 2 - Inserisco le 2 variabili fizz e buzz contenenti i numeri 3 e 5 e le inizializzo;
 let fizz = 3;
 let buzz = 5;
+
+// 3 - Creo il ciclo "for" dove inserisco le variabili per risolvere il problema;
+for (let i = firstNumber; i <= lastNumber; i++) {
+    if (i % fizz == 0 && i % buzz == 0) {
+        console.log ("FizzBuzz");
+    }
+
+    else if (i % fizz == 0) {
+        console.log("Fizz");
+    }
+
+    else if (i % buzz == 0) {
+        console.log("Buzz");
+    }
+
+    else {
+        console.log (i);
+    }
+}
